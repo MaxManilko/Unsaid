e6d064e0b04b78dbb195d489e2626903badf4508
Max Manilko <manilko330@gmail.com>
2026-05-14 23:37:54 +0300

    8-9

--- server.js
+++ server.js
@@ -2,10 +2,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
 
 
 // Підключаємось до основної БД (тільки якщо не в режимі тестування)
