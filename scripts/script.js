35709513dc1cc87dec366027bf17f4382e41eeec
Max Manilko <manilko330@gmail.com>
2026-05-19 22:58:17 +0300

    server

--- scripts/script.js
+++ scripts/script.js
@@ -162,7 +162,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	
     // --- 3. БАЗА ДАНИХ (Відправка і завантаження) ---
-    const API_URL = 'http://localhost:3000/api/messages';
+  const API_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
+    ? 'http://localhost:3000/api/messages'
+    : '/api/messages';
+
 
     // Відправка повідомлення
     const submitBtn = document.querySelector('.submit-form .form-submit');
